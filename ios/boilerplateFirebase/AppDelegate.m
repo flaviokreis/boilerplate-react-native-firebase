888f8598a5d22816d0384548c6ff5747130f1936
Flavio Kreis <flaviokreis@gmail.com>
2018-04-22 14:11:37 -0300

    Added react native firebase, iOS configuration

--- ios/boilerplateFirebase/AppDelegate.m
+++ ios/boilerplateFirebase/AppDelegate.m
@@ -7,6 +7,7 @@
 
 #import "AppDelegate.h"
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>
 
 #import "RCCManager.h"
 
@@ -16,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  
   NSURL *jsCodeLocation;
 #ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
